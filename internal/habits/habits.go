45f3a206a28034e340d85bda19d32dc9d1db8f0a
Utku Ufuk <utkuufuk@gmail.com>
2020-06-07 02:31:56 +0300

    add comments

--- internal/habits/habits.go
+++ internal/habits/habits.go
@@ -31,26 +31,10 @@ func (s source) FetchNewCards(ctx context.Context, cfg config.SourceConfig) ([]t
 		return nil, fmt.Errorf("could not fetch habits: %w", err)
 	}
 
-	fmt.Println(habits)
 	return toCards(habits, cfg.Label)
 }
 
-func toCards(habits map[interface{}]interface{}, label string) (cards []trello.Card, err error) {
-	for habit, state := range habits {
-		if fmt.Sprintf("%v", state) != "" {
-			continue
-		}
-
-		c, err := trello.NewCard(fmt.Sprintf("%v", habit), label, "", nil)
-		if err != nil {
-			return nil, fmt.Errorf("could not create habit card: %w", err)
-		}
-
-		cards = append(cards, c)
-	}
-	return cards, nil
-}
-
+// fetchHabits retrieves the state of today's habits from the spreadsheet
 func (s source) fetchHabits() (map[interface{}]interface{}, error) {
 	today := time.Now()
 	rangeName := fmt.Sprintf("%s %d!B1:Z%d", today.Month().String()[:3], today.Year(), today.Day()+3)
@@ -74,4 +58,21 @@ func (s source) readCells(rangeName string) ([][]interface{}, error) {
 		return nil, fmt.Errorf("could not read cells: %w", err)
 	}
 	return resp.Values, nil
+}
+
+// toCards returns a slice of trello cards from the given habits which haven't been marked today
+func toCards(habits map[interface{}]interface{}, label string) (cards []trello.Card, err error) {
+	for habit, state := range habits {
+		if fmt.Sprintf("%v", state) != "" {
+			continue
+		}
+
+		c, err := trello.NewCard(fmt.Sprintf("%v", habit), label, "", nil)
+		if err != nil {
+			return nil, fmt.Errorf("could not create habit card: %w", err)
+		}
+
+		cards = append(cards, c)
+	}
+	return cards, nil
 }
