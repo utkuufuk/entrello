6f68c3c57c1df75b7c2fb808050f50c2588db48a
Utku Ufuk <utkuufuk@gmail.com>
2021-09-29 21:20:03 +0300

    Get rid of hardcoded source keys from configuration (#44)

--- cmd/entrello/main.go
+++ cmd/entrello/main.go
@@ -16,44 +16,36 @@ var (
 )
 
 func main() {
-	// read configuration file path
 	var configFile string
 	flag.StringVar(&configFile, "c", "config.yml", "config file path")
 	flag.Parse()
 
-	// read config params
 	cfg, err := config.ReadConfig(configFile)
 	if err != nil {
 		log.Fatalf("Could not read configuration: %v", err)
 	}
 
-	// get a system logger instance
 	logger = syslog.NewLogger(cfg.Telegram)
 
-	// get current time for the configured location
 	loc, err := time.LoadLocation(cfg.TimezoneLocation)
 	if err != nil {
 		logger.Fatalf("Invalid timezone location: %v", loc)
 	}
 
-	// get a list of sources and the corresponding labels for each source
 	sources, labels := getSources(cfg.Sources, time.Now().In(loc))
 	if len(sources) == 0 {
 		return
 	}
 
-	// initialize the Trello client
 	client, err := trello.NewClient(cfg.Trello)
 	if err != nil {
 		logger.Fatalf("Could not create trello client: %v", err)
 	}
 
-	// load Trello cards from the board with relevant labels
 	if err := client.LoadBoard(labels); err != nil {
 		logger.Fatalf("Could not load existing cards from the board: %v", err)
 	}
 
-	// fetch new cards from each source and handle the new & stale ones
 	var wg sync.WaitGroup
 	wg.Add(len(sources))
 	for _, src := range sources {
